1a88b5d9c4b8f0dfb3e9cba327135eedbec635c7
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-03-31 17:21:21 -0700

    fix interviewer not selected case in validate function in Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -25,6 +25,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Interviewer must be selected");
+      return;
+    }
     setError("");
     props.onSave(student, interviewer);
   }
